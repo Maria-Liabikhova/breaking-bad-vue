3bdb4d8dbf874433018299226547f0c67186ff08
unknown <mashagi@yandex.ru>
2020-07-15 17:22:00 +0300

    src/components/blocks added

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import './assets/main.sass'
+import './assets/styles/main.sass'
 
 Vue.config.productionTip = false
 
