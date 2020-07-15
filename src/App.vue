40d8c4ef73dc948f914d3ba2d91ab340191af768
unknown <mashagi@yandex.ru>
2020-07-15 20:05:27 +0300

    add - move charactersa to page characters

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,12 @@
 <template>
-  <div id="app">
-    <page-header></page-header>
-    <div class="row">
-      <div class="col-md-3">
-        <sidebar></sidebar>
-      </div>
-      <div class="col-md-9">
-        <gallery></gallery>
-      </div>
-    </div>
+  <div id='app'>
+    <router-view />
   </div>
 </template>
 
 <script>
-import Header from '@/components/blocks/Header'
-import Sidebar from '@/components/blocks/Sidebar'
-import Gallery from '@/components/blocks/Gallery'
-
 export default {
   name: 'app',
-  components: {
-    'page-header': Header,
-    sidebar: Sidebar,
-    gallery: Gallery
-  },
   data() {
     return {}
   }
---
+++ src/components/pages/Characters.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <page-header></page-header>
+    <div class="row">
+      <div class="col-md-3">
+        <sidebar></sidebar>
+      </div>
+      <div class="col-md-9">
+        <h1 class="text text--h1">Characters</h1>
+        <gallery></gallery>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Header from '@/components/blocks/Header'
+import Sidebar from '@/components/blocks/Sidebar'
+import Gallery from '@/components/blocks/Gallery'
+export default {
+  name: 'Characters',
+  components: {
+    'page-header': Header,
+    sidebar: Sidebar,
+    gallery: Gallery
+  },
+  data() {
+    return {}
+  }
+}
+</script>
