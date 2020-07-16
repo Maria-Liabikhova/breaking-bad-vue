1efd4de6256d2107349df2e48c91ff6d396786d7
unknown <mashagi@yandex.ru>
2020-07-16 20:14:23 +0300

    characters data removed fron gallery to page characters

--- src/components/blocks/Gallery.vue
+++ src/components/blocks/Gallery.vue
@@ -22,46 +22,14 @@
 <script>
 export default {
   name: 'gallery',
-  data() {
-    return {
-      characters: [
-        {
-          imgSrc:
-            'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
-          nick: 'Heisenberg1',
-          name: 'Walter White1',
-          birthday: '09-07-1958'
-        },
-        {
-          imgSrc:
-            'https://majdkhatib.com/wp-content/uploads/2018/03/Breaking-Bad-Walter-White1.jpg?x72778',
-          nick: 'Heisenberg2',
-          name: 'Walter White2',
-          birthday: '09-07-1958'
-        },
-        {
-          imgSrc:
-            'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
-          nick: 'Heisenberg3',
-          name: 'Walter White3',
-          birthday: '09-07-1958'
-        },
-        {
-          imgSrc:
-            'https://majdkhatib.com/wp-content/uploads/2018/03/Breaking-Bad-Walter-White1.jpg?x72778',
-          nick: 'Heisenberg4',
-          name: 'Walter White4',
-          birthday: '09-07-1958'
-        },
-        {
-          imgSrc:
-            'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
-          nick: 'Heisenberg5',
-          name: 'Walter White5',
-          birthday: '09-07-1958'
-        }
-      ]
+  props: {
+    characters: {
+      type: Array,
+      required: true
     }
+  },
+  data() {
+    return {}
   }
 }
 </script>
--- src/components/pages/Characters.vue
+++ src/components/pages/Characters.vue
@@ -7,7 +7,7 @@
       </div>
       <div class="col-md-9">
         <h1 class="text text--h1">Characters</h1>
-        <gallery></gallery>
+        <gallery :characters="characters"></gallery>
       </div>
     </div>
   </section>
@@ -17,6 +17,7 @@
 import Header from '@/components/blocks/Header'
 import Sidebar from '@/components/blocks/Sidebar'
 import Gallery from '@/components/blocks/Gallery'
+
 export default {
   name: 'Characters',
   components: {
@@ -25,7 +26,45 @@ export default {
     gallery: Gallery
   },
   data() {
-    return {}
+    return {
+      characters: [
+        {
+          imgSrc:
+            'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
+          nick: 'Heisenberg1',
+          name: 'Walter White1',
+          birthday: '09-07-1958'
+        },
+        {
+          imgSrc:
+            'https://majdkhatib.com/wp-content/uploads/2018/03/Breaking-Bad-Walter-White1.jpg?x72778',
+          nick: 'Heisenberg2',
+          name: 'Walter White2',
+          birthday: '09-07-1958'
+        },
+        {
+          imgSrc:
+            'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
+          nick: 'Heisenberg3',
+          name: 'Walter White3',
+          birthday: '09-07-1958'
+        },
+        {
+          imgSrc:
+            'https://majdkhatib.com/wp-content/uploads/2018/03/Breaking-Bad-Walter-White1.jpg?x72778',
+          nick: 'Heisenberg4',
+          name: 'Walter White4',
+          birthday: '09-07-1958'
+        },
+        {
+          imgSrc:
+            'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
+          nick: 'Heisenberg5',
+          name: 'Walter White5',
+          birthday: '09-07-1958'
+        }
+      ]
+    }
   }
 }
 </script>
