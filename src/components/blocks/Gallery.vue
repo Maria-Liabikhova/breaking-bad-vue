99002019cf43d218455050e802b3e5ed46db75ec
unknown <mashagi@yandex.ru>
2020-07-17 18:57:40 +0300

    add - create difference between seasons page and character page in cards bottom

--- src/components/blocks/Gallery.vue
+++ src/components/blocks/Gallery.vue
@@ -1,17 +1,26 @@
 <template>
   <section class="gallery">
     <div class="row">
-      <div v-for="character in characters" :key="character.id" class= "col-md-3">
+      <!-- Критерий "character" поменин на "quality", чтобы избегать путаницы -->
+      <div v-for="quality in characters" :key="quality.id" class= "col-md-3">
         <!-- gallery__card -->
         <div class="gallery__card" :class="{'gallery__card--season': $route.path ==='/seasons'}">
           <div class="gallery__img-wrap" :class="{'gallery__img-wrap--season' : $route.path === '/seasons'}">
-            <img :src="character.imgSrc" alt="" class="gallery__img" />
+            <img :src="quality.imgSrc" alt="" class="gallery__img" />
           </div>
-          <div class="gallery__bottom" :class="{'gallery__bottom--season' : $route.path ==='/seasons'}">
-            <h3 class="gallery__nick"> nick: {{character.nickname}}</h3>
-            <p class="gallery__name">name: {{ character.name }}</p>
-            <p class="gallery__date">birthday: {{character.birthday}}</p>
+
+          <div  v-if="$route.pass ==='/'" class="gallery__bottom">
+            <h3 class="gallery__nick"> nick: {{quality.nickname}}</h3>
+            <p class="gallery__name">name: {{ quality.name }}</p>
+            <p class="gallery__date">birthday: {{quality.birthday}}</p>
           </div>
+
+          <div v-else class="gallery__bottom gallery__bottom--season">
+            <h3 class="gallery__nick"> air_date: {{quality.air_date}}</h3>
+            <p class="gallery__name">characters: {{quality.characters }}</p>
+            <p class="gallery__date">episode: {{quality.episode}}</p>
+          </div>
+
         </div>
         <!-- gallery__card END -->
       </div>
