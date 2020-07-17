fe2653a2f747d1d4181d4d0558366cb67b644b4a
unknown <mashagi@yandex.ru>
2020-07-17 18:21:52 +0300

    fix - add styles in page seasons

--- src/components/blocks/Gallery.vue
+++ src/components/blocks/Gallery.vue
@@ -3,11 +3,11 @@
     <div class="row">
       <div v-for="character in characters" :key="character.id" class= "col-md-3">
         <!-- gallery__card -->
-        <div class="gallery__card">
-          <div class="gallery__img-wrap">
+        <div class="gallery__card" :class="{'gallery__card--season': $route.path ==='/seasons'}">
+          <div class="gallery__img-wrap" :class="{'gallery__img-wrap--season' : $route.path === '/seasons'}">
             <img :src="character.imgSrc" alt="" class="gallery__img" />
           </div>
-          <div class="gallery__bottom">
+          <div class="gallery__bottom" :class="{'gallery__bottom--season' : $route.path ==='/seasons'}">
             <h3 class="gallery__nick"> nick: {{character.nickname}}</h3>
             <p class="gallery__name">name: {{ character.name }}</p>
             <p class="gallery__date">birthday: {{character.birthday}}</p>
