a5660cfa946aa3213c7df2db86822aa756f6ae88
unknown <mashagi@yandex.ru>
2020-07-20 19:56:42 +0300

    add - orange header  bottom for page 'episodes'

--- src/components/blocks/Header.vue
+++ src/components/blocks/Header.vue
@@ -1,11 +1,12 @@
 <template>
-  <header class="header">
+  <header class="header" :class="{ 'header__green-bottom' : $route.path === '/episodes'}">
+  <!-- <header class="header" :class="{ 'header__green-bottom' : $route.path === '/episodes'}" :class="{ 'header__green-bottom' : $route.path === '/quotes'}" :class="{ 'header__green-bottom' : $route.path === '/deaths'}"> -->
     <div class="container">
       <nav class="nav nav--navbar">
         <router-link to="/" :class="{ active: $route.path === '/'}" class="link link--navbar">characters</router-link>
         <router-link to="/seasons" :class="{ active: $route.path === '/seasons'}" class="link link--navbar">seasons</router-link>
         <router-link to="/episodes" :class="{ active: $route.path === '/episodes'}" class="link link--navbar">episodes</router-link>
-        <router-link to="/quotes" :class="{ active: $route.path === '/quotes'}" class=" link link--navbar">quotes</router-link>
+        <router-link to="/quotes" :class="{ active: $route.path === '/quotes'}" class="link link--navbar">quotes</router-link>
         <router-link to="/deaths" :class="{ active: $route.path === '/deaths'}" class="link link--navbar">deaths</router-link>
       </nav>
     </div>
